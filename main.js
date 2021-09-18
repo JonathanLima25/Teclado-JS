aafa43747ae9dd60428767e903bc415948c992b3
JonathanLima25 <jonathan.ferreira6@fatec.sp.gov.br>
2021-09-17 22:40:46 -0300

    mexendo no javascript

---
+++ main.js
@@ -0,0 +1,15 @@
+// get all keys
+const keys = document.querySelectorAll(".key")
+
+// play notes
+function playNote(event) {
+    // keyCode
+    // typed or pressed key
+    // if key exist
+    // play audio
+}
+
+// click with mouse
+
+// keyboard type
+window.addEventListener("keydown", playNote)
